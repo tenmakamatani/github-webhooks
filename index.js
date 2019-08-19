c9e6b76eb96ce7f36160038287f65b16234cd259
tenmakamatani <tenmakamatani0@gmail.com>
2019-08-19 13:44:42 +0900

    [add]body-parser追加

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+// body-parserでPOSTリクエストを処理
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.post("/post", (req, res) => {
-    console.log(req);
+    console.log(req.body);
 });
 
 app.listen(PORT, () => {
